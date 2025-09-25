0362a1e4cc6efae00e2fa46e53e4144a3bbba1b9
sonali-varshney <44549506+sonali-varshney@users.noreply.github.com>
2025-09-25 17:54:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "instance_name" {
   description = "Value of the EC2 instance's Name tag."
   type        = string
-  default     = "learning-terraform"
+  default     = "learning-terraforms"
 }
 
 variable "instance_type" {
